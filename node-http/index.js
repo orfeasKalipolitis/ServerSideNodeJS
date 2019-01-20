697947e79abed10c2dba9ec5992e2735cf729cf2
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2019-01-20 15:42:29 +0200

    Added some functionality to the server

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,41 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
+    console.log(`Request for ${req.url} by method ${req.method}`);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello World</h1></body></html>');
+    res.setHeader('Content-Tyep', 'text/html');
+    if (req.method === 'GET') {
+        var fileUrl;
+        if (req.url === '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+
+        if (fileExt === '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.end('<html><body><h1>Error 404! File not found!</h1></body></html>');
+                    return;
+                }
+
+                res.statusCode = 200;
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.statusCode = 404;
+            res.end('<html><body><h1>Error 404! File not HTML!</h1></body></html>');
+        }
+    } else {
+        res.statusCode = 404;
+        res.end(`<html><body><h1>Error 404! Method: ${req.method} not supported yet!</h1></body></html>`);
+    }
 });
 
 server.listen(port, hostname, () => {
