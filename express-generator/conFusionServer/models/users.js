c55cea57df7b3bafcf5d06418cd3a1d76a151773
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2019-01-26 19:03:59 +0200

    Implemented basic authentication.

---
+++ express-generator/conFusionServer/models/users.js
@@ -0,0 +1,14 @@
+//  Imports
+const mongoose  = require('mongoose');
+const Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+const User = new Schema({
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+User.plugin(passportLocalMongoose);
+
