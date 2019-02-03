ab8c8bf2d888e3bed6e36bd89ddc31f0edbcc8cc
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2019-02-03 17:11:39 +0200

    Added ImageUpload to server

--- express-generator/conFusionServer/app.js
+++ express-generator/conFusionServer/app.js
@@ -28,6 +28,7 @@ var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 var app = express();
 
@@ -60,6 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
