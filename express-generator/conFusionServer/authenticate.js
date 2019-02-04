5c6f7ec3ce9127501de5dbbefc90e26570cf8f39
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2019-02-04 18:27:18 +0200

    OAuth for facebook implemented

--- express-generator/conFusionServer/authenticate.js
+++ express-generator/conFusionServer/authenticate.js
@@ -1,10 +1,15 @@
+//  Import modules
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var FacebookTokenStrategy = require('passport-facebook-token');
 
+//  Import user model
 var User = require('./models/user');
+
+//  Import config file
 var config = require('./config.js');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
@@ -42,3 +47,30 @@ exports.verifyAdmin = (req, res, next) => {
     res.statusCode = 403;
     return next(new Error('You are not authorized to perform this operation!'));
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
--- express-generator/conFusionServer/config.js
+++ express-generator/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '315562852430793', 
+        clientSecret: '6bcf8c1dc248b5b852566aba748a5816' 
+    }
 }
